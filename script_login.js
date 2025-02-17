fb1364053b01868f4cb2ae3e57a83e218380d6d3
gagliardi-ga <gabrielgagliardi98@gmail.com>
2025-02-17 12:21:13 -0300

    Update script_login.js
    Alteração das variáveis de ambiente do script_login

--- script_login.js
+++ script_login.js
@@ -9,12 +9,12 @@ import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/fir
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "process.env.API_KEY",
+  authDomain: "process.env.AUTH_DOMAIN",
+  projectId: "process.env.PROJECT_ID",
+  storageBucket: "process.env.STORAGE_BUCKET",
+  messagingSenderId: "process.env.MESSAGING_SENDER_ID",
+  appId: "process.env.APP_ID"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
