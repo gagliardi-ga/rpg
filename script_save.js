fc95b17e315795781f613c6741096f7f3eb902ca
gagliardi-ga <gabrielgagliardi98@gmail.com>
2025-02-17 12:46:50 -0300

    Update script_save.js

--- script_save.js
+++ script_save.js
@@ -5,12 +5,12 @@ import { getFirestore, doc, setDoc, getDoc, addDoc, collection } from "https://w
 const userEmail = localStorage.getItem("userEmail");
 
 const firebaseConfig = {
-    apiKey: "process.env.API_KEY",
-    authDomain: "process.env.AUTH_DOMAIN",
-    projectId: "process.env.PROJECT_ID",
-    storageBucket: "process.env.STORAGE_BUCKET",
-    messagingSenderId: "process.env.MESSAGING_SENDER_ID",
-    appId: "process.env.APP_ID"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
   };
 
 const app = initializeApp(firebaseConfig);
